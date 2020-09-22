906862acb0de8928e58ea86857d5a5a3c4cf4bbd
Shailesh Aher <shaileshaher07@gmail.com>
2020-09-22 18:51:08 +0530

    Initial commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Shailesh Aher on 22/09/20.
+//  Copyright © 2020 Shailesh Aher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
